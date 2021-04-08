f1e5caf27280edb173a9df7184da25d2af8a15ca
内田洋介 <ee0200625@gmail.com>
2021-04-08 18:38:04 +0900

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,14 +1,14 @@
 variable "resource_group" {
   description = "The resource group"
-  default = "CHANGE_ME_RESOURCE_GROUP"
+  default = "UchidaRg"
 }
 
 variable "application_name" {
   description = "The Spring Boot application name"
-  default     = "CHANGE_ME_APP_NAME"
+  default     = "UchidaWinJava"
 }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
-  default     = "westeurope"
+  default     = "japaneast"
 }
